f08b50d26d402c7871dcf2bdf2816948c71be81c
Michael Miles <boat081062@gmail.com>
2015-03-18 05:54:34 -0500

    Space-Shooter Videos 8 and 9
    Created boundary for destroying objects as they leave the game area and
    created an asteroid hazard with destroy scripting.

---
+++ Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour
+{
+    // Declaration of public variables
+    public float tumble;
+
+    // Start is called just before any of the Update methods is called the first time (Since v1.0)
+    public void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = Random.insideUnitSphere * tumble;
+    }
+
+
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+
+    // Update is called every frame, if the MonoBehaviour is enabled (Since v1.0)
+    public void Update()
+    {
+        transform.Rotate(new Vector3(15, 20, 45) * Time.deltaTime);
+    }
+}
