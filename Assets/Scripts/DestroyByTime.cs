ecc52a95cc31ea08de2b3b79549a8ebac1ec815b
Michael Miles <boat081062@gmail.com>
2015-03-19 05:44:03 -0500

    Space-Shooter Videos 12 and 13
    Added multiple waves and in game audio for explosions, weapons and
    background.

---
+++ Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    // Declaration of public variables
+    public float lifeTime;
+
+    // Start is called just before any of the Update methods is called the first time (Since v1.0)
+    public void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class GameController : MonoBehaviour
 {
     // Declaration of public variables
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
     public GameObject hazard;
     public Vector3 spawnValues;
 
     // Start is called just before any of the Update methods is called the first time (Since v1.0)
     public void Start()
     {
-        SpawnWaves();
+        StartCoroutine (SpawnWaves());
     }
     
-    void SpawnWaves()
+    IEnumerator SpawnWaves()
     {
-        // Declaration of function variables
-        Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
-        Quaternion spawnRotation = Quaternion.identity;
+        yield return new WaitForSeconds(startWait);
 
-        Instantiate(hazard, spawnPosition, spawnRotation);
+        while(true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                // Declaration of function variables
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+        }
     }
 }
