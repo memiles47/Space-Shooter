2b4830a9edd12af69ed8af352fd843355ce0f91b
Michael Miles <boat081062@gmail.com>
2015-03-22 09:21:44 -0500

    Space-Shooter Videos 15 and 16
    Complete game and build to web player

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -38,6 +38,7 @@ public class DestroyByContact : MonoBehaviour
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, transform.position, transform.rotation);
+            gameController.GameOver();
         }
         gameController.AddScore(scoreValue);
         Destroy(other.gameObject);
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -14,17 +14,42 @@ public class GameController : MonoBehaviour
 
     // GUI Variables
     public GameObject refScoreText;
+    public GameObject refRestartText;
+    public GameObject refGameOverText;
+
+    // Declaration of Private variables
+    private bool gameOver;
+    private bool restart;
     private int score;
     private Text scoreText;
+    private Text restartText;
+    private Text gameOverText;
 
 
     // Start is called just before any of the Update methods is called the first time (Since v1.0)
     public void Start()
     {
+        gameOver = false;
+        restart = false;
         score = 0;
+        restartText = refRestartText.GetComponent<Text>();
+        gameOverText = refGameOverText.GetComponent<Text>();
         scoreText = refScoreText.GetComponent<Text>();
+        restartText.text = "";
+        gameOverText.text = "";
         UpdateScore();
         StartCoroutine (SpawnWaves());
+    }
+
+    public void Update()
+    {
+        if(restart)
+        {
+            if(Input.GetKeyDown(KeyCode.R))
+            {
+                Application.LoadLevel (Application.loadedLevel);
+            }
+        }
     }
 
     IEnumerator SpawnWaves()
@@ -41,6 +66,13 @@ public class GameController : MonoBehaviour
 
                 Instantiate(hazard, spawnPosition, spawnRotation);
                 yield return new WaitForSeconds(spawnWait);
+
+                if (gameOver)
+                {
+                    restartText.text = "Prss 'R' for Restart";
+                    restart = true;
+                    break;
+                }
             }
             yield return new WaitForSeconds(waveWait);
         }
@@ -56,5 +88,11 @@ public class GameController : MonoBehaviour
     {
         scoreText.text = "Score: " + score;
 
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over";
+        gameOver = true;
     }
 }
